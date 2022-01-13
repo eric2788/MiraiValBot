ea72447ea427a7f81c1ffd075aadbe5e6e031eec
eric2788 <tsukiko852@gmail.com>
2022-01-13 21:32:09 +0800

    update

---
+++ modules/command/panic_test.go
@@ -0,0 +1,21 @@
+package command
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPanic(t *testing.T) {
+	invoke("A")
+	invoke("B")
+	invoke("C")
+}
+
+func invoke(t string) {
+	defer func() {
+		fmt.Println(recover())
+	}()
+	if t == "B" {
+		panic("B!")
+	}
+}
