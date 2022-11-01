d69518469402dcce0bb034853c3598eedddd2acd
sysnapse <eric1008818@gmail.com>
2022-11-01 17:38:41 +0800

    remove unused log

--- aichat/xiaoai.go
+++ aichat/xiaoai.go
@@ -2,7 +2,6 @@ package aichat
 
 import (
 	"errors"
-	"fmt"
 	"regexp"
 	"strings"
 
@@ -29,7 +28,7 @@ func (ai *XiaoAi) Reply(msg string) (string, error) {
 	if bot.Instance != nil {
 		nick = bot.Instance.Nickname
 	}
-	
+
 	plain := strings.ReplaceAll(string(data), "\r\n", "")
 	replaced := xiaoAiWarningMsg.ReplaceAll([]byte(plain), []byte(""))
 	reply := replaces(string(replaced), map[string]string{
@@ -37,7 +36,7 @@ func (ai *XiaoAi) Reply(msg string) (string, error) {
 		"小爱":     nick,
 		"小米智能助理": "爹",
 	})
-	if reply == "" || reply == "<html>" {
+	if reply == "" {
 		return "", errors.New("无法获取回复讯息")
 	}
 	return reply, nil
