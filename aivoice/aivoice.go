92e675d288cfda7c172712b2f6a41f1a1575833e
sysnapse <eric1008818@gmail.com>
2022-11-04 10:09:36 +0800

    added log message for better debug

--- aivoice/aivoice.go
+++ aivoice/aivoice.go
@@ -8,10 +8,13 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/Logiase/MiraiGo-Template/utils"
 	"github.com/eric2788/go-silk/multiplat"
 	ffmpeg_go "github.com/u2takey/ffmpeg-go"
 	silk "github.com/wdvxdr1123/go-silk"
 )
+
+var logger = utils.GetModuleLogger("valbot.aivoice")
 
 const (
 	AudioSamplingRateMP3  = "22050"
@@ -25,7 +28,7 @@ func WavToSilk(b []byte) (data []byte, err error) {
 	tempName := hex.EncodeToString(md[:])
 
 	wav, pcm := tempName+".wav", tempName+".pcm"
-
+	logger.Infof("writing file: %s", wav)
 	err = os.WriteFile(wav, b, os.ModePerm)
 	if err != nil {
 		return nil, err
@@ -33,7 +36,9 @@ func WavToSilk(b []byte) (data []byte, err error) {
 	defer os.Remove(wav)
 
 	// 2.转换pcm
+	logger.Infof("converting to pcm...")
 	cmd := exec.Command("ffmpeg", "-i", wav, "-f", "s16le", "-ar", "24000", "-ac", "1", pcm)
+	logger.Infof("converted to pcm: %s", pcm)
 	multiplat.HideWindow(cmd)
 	if err = cmd.Run(); err != nil {
 		return nil, err
@@ -43,7 +48,14 @@ func WavToSilk(b []byte) (data []byte, err error) {
 	if err != nil {
 		return nil, err
 	}
-	return silk.EncodePcmBuffToSilk(pcmByte, 24000, 24000, true)
+	logger.Infof("converting to silk...")
+	defer func ()  {
+		if err == nil {
+			logger.Infof("converted to silk: %s", tempName+".silk")
+		}		
+	}()
+	data, err = silk.EncodePcmBuffToSilk(pcmByte, 24000, 24000, true)
+	return
 }
 
 // WavToAmr Wav To Amr file
@@ -55,14 +67,14 @@ func WavToAmr(b []byte) (data []byte, err error) {
 	if err != nil {
 		return nil, err
 	}
-	log.Println("writing file: ", f.Name())
+	logger.Info("writing file: ", f.Name())
 	_, err = f.Write(b)
 	f.Close()
 	defer removeFile(f.Name())
 	if err != nil {
 		return nil, err
 	}
-	log.Println("converting to amr: ", f.Name())
+	logger.Info("converting to amr: ", f.Name())
 	// Convert to AMR
 	comm := ffmpeg_go.Input(name+".wav").Output(name+".amr", ffmpeg_go.KwArgs{"ar": AudioSamplingRateAMR, "ab": AudioBitRate})
 	//comm := exec.Command("ffmpeg", "-i", "./"+name+".wav", "-ab", AudioBitRate, "-ar", AudioSamplingRateAMR, name+".amr")
@@ -71,7 +83,7 @@ func WavToAmr(b []byte) (data []byte, err error) {
 	}
 	defer removeFile(name + ".amr")
 	data, err = os.ReadFile(name + ".amr")
-	log.Println("successfully converted to amr")
+	logger.Info("successfully converted to amr")
 	return
 }
 
