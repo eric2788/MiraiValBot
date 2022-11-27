c7f08a3f611a65b54100edc502e623d09b0d9550
eric2788 <tsukiko852@gmail.com>
2022-11-27 18:11:36 +0800

    fix test error

--- services/ai/novelai_test.go
+++ services/ai/novelai_test.go
@@ -9,7 +9,7 @@ func TestGetNovelAI8zywImage(t *testing.T) {
 	url, err := GetNovelAI8zywImage(
 		New8zywPayload(
 			"1girl, best quality, masterpiece, cat ears, solo",
-			false,
+			WithoutR18,
 		),
 	)
 	if err != nil {
