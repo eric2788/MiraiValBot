19ae88aeae99e89e163ff932f515d567d21faacc
Eric Lam <tsukiko852@gmail.com>
2022-12-06 08:08:50 +0000

    better common url parsing

--- modules/urlparser/common.go
+++ modules/urlparser/common.go
@@ -3,14 +3,14 @@ package urlparser
 import (
 	"encoding/base64"
 	"fmt"
-	uurl "net/url"
+	"net/http"
 	"strings"
 
 	"github.com/Mrs4s/MiraiGo/client"
 	"github.com/Mrs4s/MiraiGo/message"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/corpix/uarand"
 	"github.com/eric2788/MiraiValBot/internal/qq"
-	"github.com/eric2788/common-utils/request"
 )
 
 type common struct {
@@ -18,11 +18,17 @@ type common struct {
 
 func (c *common) ParseURL(url string) Broadcaster {
 	return func(bot *client.QQClient, event *message.GroupMessage) error {
-		content, err := request.GetHtml(url)
+		req, err := http.NewRequest(http.MethodGet, url, nil)
 		if err != nil {
-			return fmt.Errorf("解析URL %s 出现错误: %v", url, err)
+			return fmt.Errorf("创建请求 %s 出现错误: %v", url, err)
 		}
-		docs, err := goquery.NewDocumentFromReader(strings.NewReader(content))
+		req.Header.Set("User-Agent", uarand.GetRandom())
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return fmt.Errorf("请求 %s 出现错误: %v", url, err)
+		}
+		defer res.Body.Close()
+		docs, err := goquery.NewDocumentFromReader(res.Body)
 		if err != nil {
 			return fmt.Errorf("解析URL %s 为 html 时出现错误: %v", url, err)
 		}
@@ -31,10 +37,23 @@ func (c *common) ParseURL(url string) Broadcaster {
 			title = docs.Find("title").Text()
 		}
 
-		thumbnail := docs.Find("meta[property='og:Image']").AttrOr("content", "")
+		desc := docs.Find("meta[property='og:description']").AttrOr("content", "")
+		if desc == "" {
+			desc = docs.Find("meta[name='description']").AttrOr("content", "")
+		}
+
+		thumbnail := docs.Find("meta[property='og:image']").AttrOr("content", "")
 		if thumbnail == "" {
 			thumbnail = docs.Find("img").AttrOr("src", "")
 		}
+
+		tags := docs.Find("meta[property='og:video:tag']").
+			FilterFunction(func(i int, s *goquery.Selection) bool {
+				return s.AttrOr("content", "") != ""
+			}).
+			Map(func(i int, s *goquery.Selection) string {
+				return s.AttrOr("content", "")
+			})
 
 		if title == "" {
 			return fmt.Errorf("无法解析网站 %s 的标题，已略过", url)
@@ -42,40 +61,53 @@ func (c *common) ParseURL(url string) Broadcaster {
 
 		msg := qq.CreateReply(event)
 		msg.Append(qq.NewTextfLn("标题: %s", title))
-		var img *message.GroupImageElement
-		if strings.HasPrefix(thumbnail, "data:image") {
-			b64 := strings.Split(thumbnail, "base64,")[1]
-			b, err := base64.StdEncoding.DecodeString(b64)
-			if err != nil {
-				logger.Errorf("URL %s 的封面base64 解析失败: %v, source: %s", url, err, thumbnail)
-			}
-			img, err = qq.NewImageByByte(b)
-			if err != nil {
-				logger.Errorf("URL %s 上传封面失败: %v", url, err)
-			}
-		} else if thumbnail != "" {
 
-			if strings.HasPrefix(thumbnail, "//") {
-				// //host.com/static/img/qq.png
-				thumbnail = "http:" + thumbnail
-			} else if strings.HasPrefix(thumbnail, "/") {
-				// /static/img/qq.png
-				u, err := uurl.Parse(url)
+		if desc != "" {
+			if len(desc) > 50 && !strings.HasSuffix(desc, "...") {
+				desc = desc[:50] + "..."
+			}
+			msg.Append(qq.NewTextfLn("简介: %s", desc))
+		}
+
+		if len(tags) > 0 {
+			msg.Append(qq.NewTextfLn("标签: %s", strings.Join(tags, ", ")))
+		}
+
+		if thumbnail != "" {
+
+			var img *message.GroupImageElement
+
+			if strings.HasPrefix(thumbnail, "data:image") {
+				b64 := strings.Split(thumbnail, "base64,")[1]
+				b, err := base64.StdEncoding.DecodeString(b64)
 				if err != nil {
-					logger.Errorf("URL %s 解析失败: %v", url, err)
-				} else {
-					thumbnail = fmt.Sprintf("http://%s%s", u.Host, thumbnail)
+					logger.Errorf("URL %s 的封面base64 解析失败: %v, source: %s", url, err, thumbnail)
+				}
+				img, err = qq.NewImageByByte(b)
+				if err != nil {
+					logger.Errorf("URL %s 上传封面失败: %v", url, err)
+				}
+			} else {
+
+				if strings.HasPrefix(thumbnail, "//") {
+					// //host.com/static/img/qq.png
+					thumbnail = "http:" + thumbnail
+				} else if strings.HasPrefix(thumbnail, "/") {
+					// /static/img/qq.png
+					thumbnail = fmt.Sprintf("http://%s%s", res.Request.URL.Host, thumbnail)
+				}
+
+				logger.Debugf("即将上传封面: %s", thumbnail)
+
+				img, err = qq.NewImageByUrl(thumbnail)
+				if err != nil {
+					logger.Errorf("上传封面 %s 失败: %v", thumbnail, err)
 				}
 			}
 
-			img, err = qq.NewImageByUrl(thumbnail)
-			if err != nil {
-				logger.Errorf("URL %s 上传封面失败: %v", url, err)
+			if img != nil {
+				msg.Append(img)
 			}
-		}
-
-		if img != nil {
-			msg.Append(img)
 		}
 
 		return qq.SendGroupMessage(msg)
