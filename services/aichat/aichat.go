87ec01968bc6167f65734e0097f4dcbf69ebd2e4
Eric Lam <tsukiko852@gmail.com>
2022-12-22 03:02:44 +0000

    added ai chat mode

--- services/aichat/aichat.go
+++ services/aichat/aichat.go
@@ -3,24 +3,21 @@ package aichat
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/Logiase/MiraiGo-Template/utils"
 )
 
 var (
-	aichats = []AIReply{
-		&XiaoAi{},
-		&QingYunKe{},
-		&TianXing{},
-		&MoliYun{},
-		&Chatgpt3{},
-	}
-
 	logger = utils.GetModuleLogger("service.aichat")
 )
 
 func GetRandomResponse(content string) (string, error) {
+
+	aichats := GetAIChats(os.Getenv("AI_CHAT_MODE"))
+
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(aichats), func(i, j int) { aichats[i], aichats[j] = aichats[j], aichats[i] })
 
@@ -38,4 +35,28 @@ func GetRandomResponse(content string) (string, error) {
 	}
 
 	return "", fmt.Errorf("所有 AI 均無法回復訊息。")
+}
+
+func GetAIChats(mode string) []AIReply {
+	switch strings.ToLower(mode) {
+	case "cn":
+		return []AIReply{
+			&XiaoAi{},
+			&QingYunKe{},
+			&TianXing{},
+			&MoliYun{},
+		}
+	case "chatgpt3":
+		return []AIReply{
+			&Chatgpt3{},
+		}
+	default:
+		return []AIReply{
+			&XiaoAi{},
+			&QingYunKe{},
+			&TianXing{},
+			&MoliYun{},
+			&Chatgpt3{},
+		}
+	}
 }
