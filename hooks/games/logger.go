aaa78a37d5305b5c0be7f52c19705cd11cf957b4
Eric Lam <tsukiko852@gmail.com>
2023-01-20 03:07:51 +0000

    added hints when game msg risked

--- hooks/games/logger.go
+++ hooks/games/logger.go
@@ -1,5 +1,18 @@
 package games
 
-import "github.com/Logiase/MiraiGo-Template/utils"
+import (
+	"github.com/Logiase/MiraiGo-Template/utils"
+	"github.com/Mrs4s/MiraiGo/message"
+	"github.com/eric2788/MiraiValBot/internal/qq"
+)
 
 var logger = utils.GetModuleLogger("valbot.games")
+
+func risky(err error) {
+	if err == nil {
+		return
+	}
+	msg := message.NewSendingMessage()
+	msg.Append(qq.NewTextf("发送游戏信息时出现错误: %v", err))
+	_ = qq.SendGroupMessage(msg)
+}
